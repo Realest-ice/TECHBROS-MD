de3498d102812ec6464c871b8b7bfed41180dda2
Realest-ice <lhordfendycool999@gmail.com>
2025-09-10 22:33:18 +0100

    Create about.js

---
+++ plugins/about.js
@@ -0,0 +1,59 @@
+/*
+Code by techbros
+
+*/
+const config = require('../config')
+const {cmd , commands} = require('../command')
+cmd({
+    pattern: "about",
+    alias: ["techbros","whois"], 
+    react: "👑",
+    desc: "get owner info",
+    category: "main",
+    filename: __filename
+},
+async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
+try{
+let about = `*Hey there, ${pushname}!* 👋🏻  
+
+Welcome to *TECHBROS-MD* — a multi-device WhatsApp bot powered by *Baileys*.  
+Fast, reliable, and built for all your automation needs.
+
+——————  
+
+*🧑🏻‍💻 Developers & Support:*  
+• https://wa.me/2349126807818 — *Realest_ice❄️*  
+• https://wa.me/2349076087791 — *VIDZ 🤟🏻*  
+
+*🌐 Official Website:*  
+https://yourwebsite.com  
+
+*💻 Source Code (GitHub):*  
+https://github.com/username/TECHBROS-MD  
+
+*✅Stay Updated 
+Join Our WhatsApp Channel
+https://whatsapp.com/channel/0029VarWtitEgGfDrNnWs83N  
+
+——————  
+
+> *© Powered by TECHBROS-MD 💙*  
+_Stay cool & stay smart_. ✌🏻`
+
+await conn.sendMessage(from,{image:{url:`https://i.ibb.co/d0Fv6ZS9/IMG-20250425-WA0004.jpg`},caption:about,
+                             contextInfo: {
+    mentionedJid: [m.sender],
+    forwardingScore: 999,
+    isForwarded: true,
+    forwardedNewsletterMessageInfo: {
+      newsletterJid: '120363344568809272@newsletter',
+      newsletterName: 'TECHBROS-MD',
+      serverMessageId: 999
+    }
+  }
+}, { quoted: mek });
+} catch (e) {
+console.log(e)
+reply(`${e}`)
+}
+})
